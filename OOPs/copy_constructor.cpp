17eda471931c0b34d46f09dfe96f6fadf5bcd40c
monk <ns708090100@gmail.com>
2026-09-04 13:23:25 +0530

    deep and shallow copy

--- OOPs/copy_constructor.cpp
+++ OOPs/copy_constructor.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,10 +8,10 @@ private:
   int health;
 
 public:
-  string name;
+  char *name;
 
   void set_health(int health) { this->health = health; }
-  void set_name(string name) { this->name = name; }
+  void set_name(char name[100]) { strcpy(this->name, name); }
   int get_health() { return this->health; }
   string get_name() { return this->name; }
 
@@ -19,25 +20,55 @@ public:
     cout << this->name << endl;
   }
 
-  Hero(){ }
+  Hero() { name = new char[100]; }
 
-  //copy constructor;
-  // Hero(Hero &any){
-  //   cout<<"copy called";
-  //   this->health = any.health;
-  //   this->name = any.name;
-  // }
+  // copy constructor;
+  //  Hero(Hero &any){
+  //    cout<<"copy called";
+  //    this->health = any.health;
+  //    this->name = any.name;
+  //  }
 };
 
 int main() {
   Hero wolf;
   wolf.set_health(60);
-  wolf.set_name("Hello wolf");
+
+  char name[10] = "nitish \n";
+  wolf.set_name(name);
+
   wolf.print_details();
 
-  cout<<endl;
-  
+  cout << endl;
+
+  // use default copy constructor
   Hero tiger(wolf);
   tiger.print_details();
 
+  // change the name of wolf
+  wolf.name[0] = 'R';
+  wolf.print_details();
+
+  // but if we now prints tiger its name will also be changed
+  tiger.print_details();
+
+  /*
+  60
+  nitish
+
+
+  60
+  nitish
+
+  60
+  Ritish
+
+  60
+  Ritish
+
+  this is because default copy constructor uses shallow copy which means same
+  adress is being used for both of the hero which cause this problem to prevent
+  this we use deep copy
+
+  */
 }
